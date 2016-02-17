eabc1cc5161fdaff6a66b99790ad241bfd969005
Oskar Niburski <oskarniburski@gmail.com>
2016-02-16 23:04:10 -0500

    Refactor

--- src/CalcView.java
+++ src/CalcView.java
@@ -21,9 +21,14 @@ public class CalcView extends JFrame
 	@SuppressWarnings("serial")
 	public CalcView(final CalcController theController)
 	{
+		super("Simple Calculator");
 		
-		super("Simple Calculator");	
-		createAndShowGUI();
+		addComponentsToPane(this);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+        this.setVisible(true);
+		
 	}
 
     public static void addComponentsToPane(Container pane) {
@@ -84,23 +89,6 @@ public class CalcView extends JFrame
 	pane.add(button, c);
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("GridBagLayoutDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
 	
 	/**
 	 * Get the string value of the user input text field.

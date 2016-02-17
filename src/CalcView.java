dcf41ba7f4b8386c0e50760772118f13f52ca2bf
Oskar Niburski <oskarniburski@gmail.com>
2016-02-16 23:00:04 -0500

    Gridlayoutbag

--- src/CalcView.java
+++ src/CalcView.java
@@ -10,6 +10,9 @@ import javax.swing.border.EmptyBorder;
 
 public class CalcView extends JFrame
 {
+	final static boolean shouldFill = true;
+    final static boolean shouldWeightX = true;
+    final static boolean RIGHT_TO_LEFT = false;
 	private static final long serialVersionUID = -8730447125113729547L;
 	
 	private JTextField userValueText;
@@ -18,48 +21,86 @@ public class CalcView extends JFrame
 	@SuppressWarnings("serial")
 	public CalcView(final CalcController theController)
 	{
-		super("Simple Calculator");		
-		userValueText = new JTextField(5);
 		
-		calcText = new JTextField(20);
-		calcText.setEditable(false);
-		
-		JPanel gui = new JPanel(new BorderLayout(6,6));
-		gui.setBorder(new EmptyBorder(5,5,5,5));
-		
-        this.setContentPane(gui);
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        gui.add(calcText, BorderLayout.NORTH);
-        gui.add(new JLabel("Calculated Value"));
-        gui.add(userValueText);
-        gui.add(new JLabel("User Input"));
-
-        // add padding around the buttons
-        JPanel panelButtons = new JPanel(new GridLayout(5,5,6,6));
-		for (int i=0; i < 10; i++) {
-			JButton oneButton = new JButton(""+i);
-	        panelButtons.add(oneButton);
-		}
-        
-        panelButtons.add(new ButtonAdapter("Sum") {
-		       public void pressed(){ theController.sum(getUserValue());}});
-        panelButtons.add(new ButtonAdapter("Subtract") {
-		       public void pressed(){ theController.subtract(getUserValue());}});
-        panelButtons.add(new ButtonAdapter("Multiply") {
-		       public void pressed(){ theController.multiply(getUserValue());}});
-        panelButtons.add(new ButtonAdapter("Divide") {
-		       public void pressed(){ theController.divide(getUserValue());}});
-        panelButtons.add(new ButtonAdapter("Clear") {
-		       public void pressed(){ theController.clear();}});
-		gui.add(panelButtons, BorderLayout.CENTER);
-		
-		
-		
-		this.pack();
-        this.setVisible(true); 
+		super("Simple Calculator");	
+		createAndShowGUI();
 	}
+
+    public static void addComponentsToPane(Container pane) {
+        if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
+
+        JButton button;
+	pane.setLayout(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
+	if (shouldFill) {
+	//natural height, maximum width
+	c.fill = GridBagConstraints.HORIZONTAL;
+	}
+
+	button = new JButton("Button 1");
+	
+	if (shouldWeightX) {
+		c.weightx = 0.5;
+	}
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.gridx = 0;
+	c.gridy = 0;
+	pane.add(button, c);
+
+	button = new JButton("Button 2");
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.weightx = 0.5;
+	c.gridx = 1;
+	c.gridy = 0;
+	pane.add(button, c);
+
+	button = new JButton("Button 3");
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.weightx = 0.5;
+	c.gridx = 2;
+	c.gridy = 0;
+	pane.add(button, c);
+
+	button = new JButton("Long-Named Button 4");
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.ipady = 40;      //make this component tall
+	c.weightx = 0.0;
+	c.gridwidth = 3;
+	c.gridx = 0;
+	c.gridy = 1;
+	pane.add(button, c);
+
+	button = new JButton("5");
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.ipady = 0;       //reset to default
+	c.weighty = 1.0;   //request any extra vertical space
+	c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+	c.insets = new Insets(10,0,0,0);  //top padding
+	c.gridx = 1;       //aligned with button 2
+	c.gridwidth = 2;   //2 columns wide
+	c.gridy = 2;       //third row
+	pane.add(button, c);
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("GridBagLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 	
 	/**
 	 * Get the string value of the user input text field.

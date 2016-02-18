37a1737b36115199ca4d117cba9b837189cf4a98
Brad Booth <brad_booth@live.com>
2016-02-17 19:02:29 -0500

    Added multiply/divide test methods

--- src/CalcControllerTest.java
+++ src/CalcControllerTest.java
@@ -8,14 +8,16 @@ import org.junit.Test;
 
 public class CalcControllerTest {
 	
-	CalcModel model = new CalcModel();
-	CalcController controller = new CalcController(model);
+	CalcModel model;
+	CalcController controller;
 	
 	
 	@Before
 	public void setUp() throws Exception 
 	{
 		//Should probably set something up here...
+		model = new CalcModel();
+		controller = new CalcController(model);
 	}
 	
 	@Test
@@ -29,7 +31,6 @@ public class CalcControllerTest {
 	@Test
 	public void sumTest()
 	{
-		
 		Stack<BigInteger> s = new Stack<BigInteger>();
 		s.add(BigInteger.valueOf(1));
 		s.add(BigInteger.valueOf(2));
@@ -39,5 +40,51 @@ public class CalcControllerTest {
 		assertEquals(BigInteger.valueOf(3), model.getCalcValue());
 
 	}
+	
+	@Test
+	public void subtractTest()
+	{
+		Stack<BigInteger> s = new Stack<BigInteger>();
+		s.add(BigInteger.valueOf(2));
+		s.add(BigInteger.valueOf(1));
+		
+		controller.subtract(s);
+		
+		assertEquals(BigInteger.valueOf(1), model.getCalcValue());
+		
+	}
+	
+	@Test
+	public void multiplicationTest()
+	{
+		Stack<BigInteger> s = new Stack<BigInteger>();
+		s.add(BigInteger.valueOf(2));
+		s.add(BigInteger.valueOf(3));
+		
+		controller.multiply(s);
+		
+		assertEquals(BigInteger.valueOf(6), model.getCalcValue());
+		
+	}
+	
+	@Test
+	public void divideTest()
+	{
+		Stack<BigInteger> s = new Stack<BigInteger>();
+		s.add(BigInteger.valueOf(10));
+		s.add(BigInteger.valueOf(5));
+		
+		controller.divide(s);
+		
+		assertEquals(BigInteger.valueOf(2), model.getCalcValue());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
